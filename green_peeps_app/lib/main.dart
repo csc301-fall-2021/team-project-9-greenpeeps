84807828a258a47e6f21e1a56f7df46251ec05c9
Nathan DeGoey <n.degoey@mail.utoronto.ca>
2021-10-19 02:10:31 -0400

    Integrated FlutterFire with Android

--- green_peeps_app/lib/main.dart
+++ green_peeps_app/lib/main.dart
@@ -1,19 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:green_peeps_app/navigation.dart';
 
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  _AppState createState() => _AppState();
+}
 
-  // This widget is the root of your application.
+class _AppState extends State<MyApp> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Show error message if initialization failed
+    if (_error) {
+      // return SomethingWentWrong();
+      print("error in building the Firebase project");
+    }
+
     return MaterialApp(home: navigation());
   }
 }
+
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(home: navigation());
+//   }
+// }
 
 // class MyHomePage extends StatefulWidget {
 //   const MyHomePage({Key? key, required this.title}) : super(key: key);
