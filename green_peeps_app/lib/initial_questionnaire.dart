b5886251e149f570e6180b5ca13ffd97a09445f3
Eryka <31933725+shishune@users.noreply.github.com>
2021-10-24 15:42:15 -0400

    edit ui design - gradient!

--- green_peeps_app/lib/initial_questionnaire.dart
+++ green_peeps_app/lib/initial_questionnaire.dart
@@ -36,6 +36,24 @@ class _InitialQuestionnaireState extends State<InitialQuestionnaire> {
     Question(id: "q3", text: "How far do you drive everyday?",
         fieldType: 0, type: 1, tags: ["Travel"],
         answers: {"default": q3AnswerDefault}),
+    Question(id: "q1", text: "Do you own a car?", fieldType: 1, type: 0,
+        tags: ["Travel"], answers: {"Yes": q1AnswerYes, "No": q1AnswerNo}),
+    Question(id: "q2", text: "What type of car do you have?",
+        fieldType: 2, type: 2, tags: ["Travel"],
+        answers: {"A good car": q2AnswerGood,
+          "An okay car": q2AnswerOkay, "A bad car": q2AnswerBad}),
+    Question(id: "q3", text: "How far do you drive everyday?",
+        fieldType: 0, type: 1, tags: ["Travel"],
+        answers: {"default": q3AnswerDefault}),
+    Question(id: "q1", text: "Do you own a car?", fieldType: 1, type: 0,
+        tags: ["Travel"], answers: {"Yes": q1AnswerYes, "No": q1AnswerNo}),
+    Question(id: "q2", text: "What type of car do you have?",
+        fieldType: 2, type: 2, tags: ["Travel"],
+        answers: {"A good car": q2AnswerGood,
+          "An okay car": q2AnswerOkay, "A bad car": q2AnswerBad}),
+    Question(id: "q3", text: "How far do you drive everyday?",
+        fieldType: 0, type: 1, tags: ["Travel"],
+        answers: {"default": q3AnswerDefault}),
   ];
   List<Response> responses = [];
 
@@ -65,34 +83,48 @@ class _InitialQuestionnaireState extends State<InitialQuestionnaire> {
 
   Widget build(BuildContext context) {
     _createResponses();
-    int i = 0;
-    return Scaffold(
-      backgroundColor: Colors.black, // TODO gradient??
-      floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            _saveResponses();
-            Navigator.popAndPushNamed(context, '/nav');
-          },
-          label: const Text("Save & Quit",
-            style:  TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontFamily: "Nunito",
-              fontWeight: FontWeight.w700,
-            ),
+    int i = 0; 
+    return SafeArea(
+      child: Container(
+        // decoration: BoxDecoration(
+        //     gradient: LinearGradient(
+        //         begin: Alignment.topCenter,
+        //         end: Alignment.bottomCenter,
+        //         colors: [Colors.black, Colors.purple])),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          floatingActionButton: FloatingActionButton.extended(
+              onPressed: () {
+                _saveResponses();
+                Navigator.popAndPushNamed(context, '/nav');
+              },
+              label: const Text("Save & Quit",
+                style:  TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontFamily: "Nunito",
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              backgroundColor: Colors.green,
           ),
-          backgroundColor: Colors.green,
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-            child: Column(
-                children: <Widget>[
-                  for(int i = 0 ; i < questions.length ; i++)
-                    // if(questions[i].fieldType == 2 && responses[i].answer != "A bad car")
-                    QuestionnaireCard(question: questions[i],
-                        response: responses[i])
-                  ]
+          body: SingleChildScrollView(
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.black, Colors.purple])),
+                child: Column(
+                    children: <Widget>[
+                      for(int i = 0 ; i < questions.length ; i++)
+                        // if(questions[i].fieldType == 2 && responses[i].answer != "A bad car")
+                        QuestionnaireCard(question: questions[i],
+                            response: responses[i])
+                      ]
+                  ),
               )
+          ),
         ),
       ),
     );
