af8c8149d735c65203b90ff1221911cf5b4bb9f3
Eryka <31933725+shishune@users.noreply.github.com>
2021-10-23 21:35:41 -0400

    add questions to initial questionnaire

--- green_peeps_app/lib/initial_questionnaire.dart
+++ green_peeps_app/lib/initial_questionnaire.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tuple/tuple.dart';
+import 'new_user_questionnaire/question.dart';
+import 'new_user_questionnaire/response.dart';
+import 'new_user_questionnaire/questionnaire_card.dart';
+
 
 class InitialQuestionnaire extends StatefulWidget {
   const InitialQuestionnaire({Key? key}) : super(key: key);
@@ -9,7 +14,37 @@ class InitialQuestionnaire extends StatefulWidget {
 
 class _InitialQuestionnaireState extends State<InitialQuestionnaire> {
   @override
+  String userID = "u1";
+
+  static Tuple2<double, List<String>> q1AnswerYes = Tuple2(1, ["q2"]);
+  static Tuple2<double, List<String>> q1AnswerNo = Tuple2(1, []);
+  static Tuple2<double, List<String>> q2AnswerGood = Tuple2(2, ["q3"]);
+  static Tuple2<double, List<String>> q2AnswerOkay = Tuple2(6, ["q3"]);
+  static Tuple2<double, List<String>> q2AnswerBad = Tuple2(10, ["q3"]);
+  static Tuple2<double, List<String>> q3AnswerDefault = Tuple2(10, []);
+
+  List<Question> questions = [
+    Question(id: "q1", text: "Do you own a car?", fieldType: 1, type: 0,
+    tags: ["Travel"], answers: {"Yes": q1AnswerYes, "No": q1AnswerNo}),
+    Question(id: "q2", text: "What type of car do you have?",
+        fieldType: 2, type: 2, tags: ["Travel"],
+        answers: {"A good car": q2AnswerGood,
+        "An okay car": q2AnswerOkay, "A bad car": q2AnswerBad}),
+    Question(id: "q3", text: "How far do you drive everyday?",
+        fieldType: 0, type: 1, tags: ["Travel"],
+        answers: {"default": q3AnswerDefault}),
+  ];
+
+
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+        children: <Widget>[
+          Column(
+            children: questions.map((question) =>
+                QuestionnaireCard(question: question,
+                response: Response(userID: userID, qID: question.getId()))).toList(),
+          )
+        ]
+    );
   }
 }
