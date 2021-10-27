c44ff0db60831d1422920b861d7696cc9fcb9f6c
Philip Cai <philip.cai@mail.utoronto.ca>
2021-10-27 02:04:26 -0400

    Fixed consumer scope

--- green_peeps_app/lib/new_user_questionnaire/initial_questionnaire.dart
+++ green_peeps_app/lib/new_user_questionnaire/initial_questionnaire.dart
@@ -19,52 +19,51 @@ class _InitialQuestionnaireState extends State<InitialQuestionnaire> {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
         create: (context) => QuestionListModel('F3Ct0WCqgIaAlkdrqE7X'),
-        child: Consumer<QuestionListModel>(
-            builder: (context, questionListModel, child) {
-          return SafeArea(
-            child: Container(
-              // decoration: BoxDecoration(
-              //     gradient: LinearGradient(
-              //         begin: Alignment.topCenter,
-              //         end: Alignment.bottomCenter,
-              //         colors: [Colors.black, Colors.purple])),
-              child: Scaffold(
-                backgroundColor: Colors.transparent,
-                floatingActionButton: FloatingActionButton.extended(
-                  onPressed: () {
-                    Navigator.popAndPushNamed(context, '/nav');
-                  },
-                  label: const Text(
-                    "Save & Quit",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontFamily: "Nunito",
-                      fontWeight: FontWeight.w700,
-                    ),
+        child: SafeArea(
+          child: Container(
+            // decoration: BoxDecoration(
+            //     gradient: LinearGradient(
+            //         begin: Alignment.topCenter,
+            //         end: Alignment.bottomCenter,
+            //         colors: [Colors.black, Colors.purple])),
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              floatingActionButton: FloatingActionButton.extended(
+                onPressed: () {
+                  Navigator.popAndPushNamed(context, '/nav');
+                },
+                label: const Text(
+                  "Save & Quit",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontFamily: "Nunito",
+                    fontWeight: FontWeight.w700,
                   ),
-                  backgroundColor: Colors.green,
                 ),
-                body: SingleChildScrollView(
-                    child: Container(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [Colors.black, Colors.purple])),
-                        child: Column(children: [
-                          for (Question question in questionListModel.questionList)
-                            QuestionnaireCard(
-                                question: question,
-                                response:
-                                    Response(userID: userID, qID: question.getId())),
-                          SizedBox(height: 800, ), // TODO remove
-                        ],
-                        ),
-                )),
+                backgroundColor: Colors.green,
               ),
+              body: SingleChildScrollView(
+                  child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.black, Colors.purple])),
+                child: Consumer<QuestionListModel>(
+                    builder: (context, questionListModel, child) {
+                  return Column(children: [
+                    for (Question question in questionListModel.questionList)
+                      QuestionnaireCard(
+                          question: question,
+                          response:
+                              Response(userID: userID, qID: question.getId())),
+                    SizedBox(height: 800), // TODO remove
+                  ]);
+                }),
+              )),
             ),
-          );
-        }));
+          ),
+        ));
   }
 }
