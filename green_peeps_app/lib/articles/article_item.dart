26a195db9ccc6ac00f0f46f2ebedc9f44da451c7
Akiki Liang <akiki.liang@mail.utoronto.ca>
2021-11-23 22:35:56 -0500

    Add featured article box to homescreen

--- green_peeps_app/lib/articles/article_item.dart
+++ green_peeps_app/lib/articles/article_item.dart
@@ -5,8 +5,9 @@ import 'package:url_launcher/url_launcher.dart';
 class ArticleItem extends StatefulWidget {
   final String title;
   final String link;
+  final String randImg;
   
-  const ArticleItem({Key? key, required this.title, required this.link}) : super(key: key);
+  const ArticleItem({Key? key, required this.title, required this.link, required this.randImg}) : super(key: key);
 
   @override 
   _ArticleItem createState() => _ArticleItem();
@@ -17,39 +18,35 @@ class _ArticleItem extends State<ArticleItem> {
   @override 
   Widget build(BuildContext context) {
     return Center(
-      child: Stack(
-        children: <Widget>[
-          Container(
-            height: 200,
-            //color: Colors.amber[500],
-          ),
-          Container(
-            padding: const EdgeInsets.all(80),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(11.0),
-              image: const DecorationImage(
-                image: NetworkImage('https://images.ctfassets.net/hrltx12pl8hq/6TOyJZTDnuutGpSMYcFlfZ/4dfab047c1d94bbefb0f9325c54e08a2/01-nature_668593321.jpg?fit=fill&w=480&h=270'),
-                fit: BoxFit.cover,
-              ),
+      child: InkWell(
+        child: Container(
+          height: 200,
+          padding: const EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            image: DecorationImage(
+              image: NetworkImage(widget.randImg),
+              fit: BoxFit.cover,
             ),
-            //color: Colors.amber[500],
-            child: Center(
-              child: InkWell(
-                child: 
-                  Text(
-                    widget.title,
-                    textAlign: TextAlign.center,
-                  ),
-                  onTap: () => launch(widget.link)
+          ),
+          //color: Colors.amber[500],
+          child: Center(
+            child: Text(
+              widget.title,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                backgroundColor: Colors.black.withOpacity(0.6),
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
-          
-        ], 
+        ),
+        onTap: () => launch(widget.link)
       )
-      
-        
+
     );
   }
 }
