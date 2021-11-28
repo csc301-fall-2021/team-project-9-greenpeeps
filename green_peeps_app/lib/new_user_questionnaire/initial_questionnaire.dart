b6402954072a50190d8828657497a94c75e6e841
Philip Cai <philip.cai@mail.utoronto.ca>
2021-11-28 03:12:04 -0500

    Finally found a solution to get the scrolly bit to stretch

--- green_peeps_app/lib/new_user_questionnaire/initial_questionnaire.dart
+++ green_peeps_app/lib/new_user_questionnaire/initial_questionnaire.dart
@@ -18,58 +18,54 @@ class _InitialQuestionnaireState extends State<InitialQuestionnaire> {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-              create: (context) => QuestionListModel('F3Ct0WCqgIaAlkdrqE7X')),
-          Provider(create: (context) => ResponseListModel())
-        ],
-        child: SafeArea(
-          child: Container(
-            // decoration: BoxDecoration(
-            //     gradient: LinearGradient(
-            //         begin: Alignment.topCenter,
-            //         end: Alignment.bottomCenter,
-            //         colors: [Colors.black, Colors.purple])),
-            child: Scaffold(
-              backgroundColor: Colors.transparent,
-              floatingActionButton: Consumer<ResponseListModel>(
-                  builder: (context, responseListModel, child) {
-                return FloatingActionButton.extended(
-                  onPressed: () {
-                    responseListModel.saveResponsesToStore();
-                    Navigator.popAndPushNamed(context, '/nav');
-                  },
-                  label: const Text(
-                    "Save & Quit",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontFamily: "Nunito",
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  backgroundColor: Colors.green,
-                );
-              }),
-              body: SingleChildScrollView(
-                  child: Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [Colors.black, Colors.purple])),
-                child: Consumer<QuestionListModel>(
-                    builder: (context, questionListModel, child) {
-                  return Column(children: [
-                    for (Future<Question?> question
-                        in questionListModel.questionList)
-                      QuestionnaireCard(question: question),
-                    SizedBox(height: 600) // TODO remove
-                  ]);
-                }),
-              )),
-            ),
-          ),
-        ));
+      providers: [
+        ChangeNotifierProvider(
+            create: (context) => QuestionListModel('F3Ct0WCqgIaAlkdrqE7X')),
+        Provider(create: (context) => ResponseListModel())
+      ],
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          floatingActionButton: Consumer<ResponseListModel>(
+              builder: (context, responseListModel, child) {
+            return FloatingActionButton.extended(
+              onPressed: () {
+                responseListModel.saveResponsesToStore();
+                Navigator.popAndPushNamed(context, '/nav');
+              },
+              label: const Text(
+                "Save & Quit",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontFamily: "Nunito",
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              backgroundColor: Colors.green,
+            );
+          }),
+          body: CustomScrollView(slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Colors.black, Colors.purple])),
+                  child: Consumer<QuestionListModel>(
+                      builder: (context, questionListModel, child) {
+                    return Column(children: [
+                      for (Future<Question?> question
+                          in questionListModel.questionList)
+                        QuestionnaireCard(question: question),
+                    ]);
+                  })),
+            )
+          ]),
+        ),
+      ),
+    );
   }
 }
