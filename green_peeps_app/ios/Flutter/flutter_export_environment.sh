2570bb9d040d966ef20088c0b60e7c1839576c81
Jeffrey Wong <jeffreywong0417@gmail.com>
2021-11-30 11:40:33 -0500

    Stage changes

--- green_peeps_app/ios/Flutter/flutter_export_environment.sh
+++ green_peeps_app/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/nathandegoey/Utils/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/nathandegoey/Documents/CSC301/green_peeps_app"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Jeffrey\csc301-greenpeeps\green_peeps_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
