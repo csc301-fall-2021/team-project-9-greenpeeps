38f290aaf83bc8ad9e9834e2d6e61c6e82edf49b
Grace Leung <greyse.leung@mail.utoronto.ca>
2021-11-30 04:24:59 -0500

    added messages for empty habit lists

--- green_peeps_app/lib/habits/my_habits_section.dart
+++ green_peeps_app/lib/habits/my_habits_section.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:green_peeps_app/habits/habit_tile_info.dart';
-import 'package:green_peeps_app/habits/habit_tile_info_dialogue.dart';
+import 'package:green_peeps_app/habits/my_habits_empty.dart';
 
 import 'package:green_peeps_app/models/habit.dart';
-import 'package:green_peeps_app/services/habit_firestore.dart';
 
 class MyHabitsSection extends StatefulWidget {
   const MyHabitsSection({Key? key}) : super(key: key);
@@ -15,7 +14,38 @@ class MyHabitsSection extends StatefulWidget {
 }
 
 class _MyHabitsSectionState extends State<MyHabitsSection> {
-  Widget _buildMyHabitsSection(BuildContext context, List<Habit> habitList) {
+  Widget _buildExistsHabits(List<Habit> habitList) {
+    return Material(
+      color: const Color.fromRGBO(248, 244, 219, 1),
+      borderRadius: BorderRadius.circular(5.0),
+      elevation: 5,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: habitList.length,
+        itemBuilder: (context, index) {
+          return Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                HabitTileInfo(
+                    habitNum: index + 1,
+                    habitName: habitList[index].title,
+                    habitDescription: habitList[index].info),
+                const Divider(
+                  color: Colors.grey,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildMyHabitsSection(BuildContext context, Widget habitListWidget) {
     return SliverList(
       delegate: SliverChildBuilderDelegate(
         (BuildContext context, int index) {
@@ -43,34 +73,7 @@ class _MyHabitsSectionState extends State<MyHabitsSection> {
                   ],
                 ),
               ),
-              Material(
-                color: const Color.fromRGBO(248, 244, 219, 1),
-                borderRadius: BorderRadius.circular(5.0),
-                elevation: 5,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: habitList.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      width: double.infinity,
-                      padding: const EdgeInsets.all(10),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          HabitTileInfo(
-                              habitNum: index + 1,
-                              habitName: habitList[index].title,
-                              habitDescription: habitList[index].info),
-                          const Divider(
-                            color: Colors.grey,
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
+              habitListWidget,
             ],
           );
         },
@@ -89,43 +92,52 @@ class _MyHabitsSectionState extends State<MyHabitsSection> {
         FirebaseFirestore.instance.collection('habits').snapshots();
 
     return StreamBuilder<DocumentSnapshot>(
-        stream: users,
-        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            List completedHabitKeys = [];
-            List<Habit> completedHabitList = [];
-            var userData = snapshot.data;
-            completedHabitKeys = userData!['userHabits'].keys.toList();
+      stream: users,
+      builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          List completedHabitKeys = [];
+          List<Habit> completedHabitList = [];
+          var userData = snapshot.data;
+          completedHabitKeys = userData!['userHabits'].keys.toList();
 
-            return StreamBuilder<QuerySnapshot>(
-              stream: habits,
-              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot2) {
-                if (snapshot2.connectionState == ConnectionState.active) {
-                  var habitData = snapshot2.data;
-                  for (var doc in habitData!.docs) {
-                    if (completedHabitKeys.contains(doc.id) && userData['userHabits'][doc.id]['completed']) {
-                      Map<String, dynamic> data =
-                          doc.data()! as Map<String, dynamic>;
-                      var newHabit = Habit(
-                          id: doc.id,
-                          title: data['title'],
-                          info: data['info'],
-                          hid: data['hid'],
-                          totalAmount: data['amount'],
-                          points: data['points'],
-                          repsLeft: userData['userHabits'][doc.id]['repsLeft']);
-                      completedHabitList.add(newHabit);
-                    }
+          return StreamBuilder<QuerySnapshot>(
+            stream: habits,
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot2) {
+              if (snapshot2.connectionState == ConnectionState.active) {
+                var habitData = snapshot2.data;
+                for (var doc in habitData!.docs) {
+                  if (completedHabitKeys.contains(doc.id) &&
+                      userData['userHabits'][doc.id]['completed']) {
+                    Map<String, dynamic> data =
+                        doc.data()! as Map<String, dynamic>;
+                    var newHabit = Habit(
+                        id: doc.id,
+                        title: data['title'],
+                        info: data['info'],
+                        hid: data['hid'],
+                        totalAmount: data['amount'],
+                        points: data['points'],
+                        repsLeft: userData['userHabits'][doc.id]['repsLeft']);
+                    completedHabitList.add(newHabit);
                   }
-                  return _buildMyHabitsSection(context, completedHabitList);
-                } else {
-                  return _buildMyHabitsSection(context, []);
                 }
-              },
-            );
-          } else {
-            return _buildMyHabitsSection(context, []);
-          }
-        });
+                return _buildMyHabitsSection(
+                    context, _buildExistsHabits(completedHabitList));
+              } else {
+                return _buildMyHabitsSection(
+                  context,
+                  const MyHabitsEmpty(),
+                );
+              }
+            },
+          );
+        } else {
+          return _buildMyHabitsSection(
+            context,
+            const MyHabitsEmpty(),
+          );
+        }
+      },
+    );
   }
 }
